57858de3cd1a351841ce1a1972da6c766743d349
148120Tran <nhattran1801@gmail.com>
2024-09-11 15:33:12 +0700

    first commit

---
+++ server/app.js
@@ -0,0 +1,85 @@
+require("dotenv").config();
+require("express-async-errors");
+// express
+
+const express = require("express");
+const app = express();
+// rest of the packages
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
+
+// database
+const connectDB = require("./db/connect");
+
+//  routers
+const authRouter = require("./routes/authRoutes");
+const userRouter = require("./routes/userRoutes");
+const productRouter = require("./routes/productRoutes");
+const reviewRouter = require("./routes/reviewRoutes");
+const orderRouter = require("./routes/orderRoutes");
+
+// middleware
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+//security middleware
+app.use(helmet());
+// app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Replace with your frontend URL
+    credentials: true,
+  })
+);
+app.use(xss());
+app.use(mongoSanitize());
+
+/* 
+helmet(): Helps secure the app by setting various HTTP headers.
+cors(): Enables Cross-Origin Resource Sharing, allowing your API to be accessed from different domains.
+xss(): Prevents cross-site scripting attacks by sanitizing user input.
+mongoSanitize(): Prevents MongoDB Operator Injection by sanitizing user input.
+*/
+
+app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
+
+//static file and upload file
+app.use(express.static("./public"));
+app.use(fileUpload());
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/products", productRouter);
+app.use("/api/v1/reviews", reviewRouter);
+app.use("/api/v1/orders", orderRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ server/models/User.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide name'],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, 'Please provide email'],
+    validate: {
+      validator: validator.isEmail,
+      message: 'Please provide valid email',
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide password'],
+    minlength: 6,
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'user'],
+    default: 'user',
+  },
+});
+
+
+//this should be using func keyword, and so the "this" will be reference to the entire page you are coding for
+//the .pre func runs before a user doc is saved to the db
+UserSchema.pre('save', async function () {
+  // console.log(this.modifiedPaths());
+  // console.log(this.isModified('name'));
+  if (!this.isModified('password')) return;
+  //if the password hasnt been modified, the func should return early and skip the rest of the code
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  //Hashes the password with the generated salt and assigns the hashed password back to the password field.
+});
+
+UserSchema.methods.comparePassword = async function (canditatePassword) {
+  const isMatch = await bcrypt.compare(canditatePassword, this.password);
+  return isMatch;
+};
+
+module.exports = mongoose.model('User', UserSchema);
